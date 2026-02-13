886b230c6d6ac7d945f09bf0f4bbd3a09ee09ee5
Serhii Pikhota <serhii.pikhota@gmail.com>
2026-02-13 19:54:05 +0000

    Build physics library with Zig

--- build.zig
+++ build.zig
@@ -4,20 +4,8 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib = b.dependency("raylib", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const pikhotics = b.createModule(.{
-        .optimize = optimize,
-        .target = target,
-        .link_libc = true,
-    });
-
-    pikhotics.addIncludePath(raylib.path("src"));
-    pikhotics.addIncludePath(raylib.path("examples"));
-    pikhotics.addIncludePath(b.path("include"));
+    const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize, .config = "-DPLATFORM_DESKTOP" });
+    const root_module = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true });
 
     var dir = try std.fs.cwd().openDir("core", .{ .iterate = true });
     defer dir.close();
@@ -27,36 +15,39 @@ pub fn build(b: *std.Build) !void {
     while (try iter.next()) |entry| {
         if (entry.kind != .file) continue;
         if (std.mem.lastIndexOf(u8, entry.name, ".c") == null) continue;
-        if (std.mem.eql(u8, entry.name, "main")) continue;
+        if (std.mem.eql(u8, entry.name, "main.c")) continue;
 
-        pikhotics.addCSourceFile(.{
+        root_module.addCSourceFile(.{
             .file = b.path(b.pathJoin(&.{ "core", entry.name })),
             .flags = flags,
         });
     }
 
     const lib = b.addLibrary(.{
-        .name = "pikhosics",
+        .name = "bandura",
         .linkage = .dynamic,
-        .root_module = pikhotics,
+        .root_module = root_module,
     });
+
+    lib.addIncludePath(raylib.path("src"));
+    lib.addIncludePath(raylib.path("examples"));
+    lib.addIncludePath(b.path("include"));
+
     lib.linkLibrary(raylib.artifact("raylib"));
 
-    const exe = b.addExecutable(.{
-        .name = "physics",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
+    if (target.result.os.tag == .linux) {
+        lib.linkSystemLibrary("m");
+        lib.linkSystemLibrary("pthread");
+        lib.linkSystemLibrary("GLX");
+        lib.linkSystemLibrary("X11");
+        lib.linkSystemLibrary("Xcursor");
+        lib.linkSystemLibrary("Xext");
+        lib.linkSystemLibrary("Xfixes");
+        lib.linkSystemLibrary("Xi");
+        lib.linkSystemLibrary("Xinerama");
+        lib.linkSystemLibrary("Xrandr");
+        lib.linkSystemLibrary("Xrender");
+    }
 
-    exe.addCSourceFile(.{ .file = b.path("core/main.c"), .flags = flags });
-    exe.addCSourceFile(.{ .file = b.path("scenarios/rigidbodies.c"), .flags = flags });
-
-    exe.linkFramework("IOKit");
-    exe.linkFramework("Cocoa");
-    exe.linkFramework("OpenGL");
-    exe.linkLibrary(lib);
-
-    b.installArtifact(exe);
+    b.installArtifact(lib);
 }
