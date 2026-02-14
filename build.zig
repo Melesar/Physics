d2be47424498d90f9220e215ac8b617df4659e8d
Serhii Pikhota <serhii.pikhota@gmail.com>
2026-02-14 13:06:11 +0100

    Automatically collect sources

--- build.zig
+++ build.zig
@@ -1,27 +1,22 @@
 const std = @import("std");
 
+const ResolvedTarget = std.Build.ResolvedTarget;
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize, .config = "-DPLATFORM_DESKTOP" });
+    const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize, .config = "-DPLATFORM_DESKTOP", .linkage = .static });
     const root_module = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true });
 
-    var dir = try std.fs.cwd().openDir("core", .{ .iterate = true });
-    defer dir.close();
-
     const flags = &.{ "-g", "-std=c99", "-Wall", "-Wextra", "-Werror", "-O0", "-fsanitize=address" };
-    var iter = dir.iterate();
-    while (try iter.next()) |entry| {
-        if (entry.kind != .file) continue;
-        if (std.mem.lastIndexOf(u8, entry.name, ".c") == null) continue;
-        if (std.mem.eql(u8, entry.name, "main.c")) continue;
+    const library_sources = try collectSources(b, "core");
+    defer b.allocator.free(library_sources);
 
-        root_module.addCSourceFile(.{
-            .file = b.path(b.pathJoin(&.{ "core", entry.name })),
-            .flags = flags,
-        });
-    }
+    root_module.addCSourceFiles(.{
+        .files = library_sources,
+        .flags = flags,
+    });
 
     const lib = b.addLibrary(.{
         .name = "bandura",
@@ -32,22 +27,46 @@ pub fn build(b: *std.Build) !void {
     lib.addIncludePath(raylib.path("src"));
     lib.addIncludePath(raylib.path("examples"));
     lib.addIncludePath(b.path("include"));
+    linkLibraries(lib, target);
 
     lib.linkLibrary(raylib.artifact("raylib"));
 
-    if (target.result.os.tag == .linux) {
-        lib.linkSystemLibrary("m");
-        lib.linkSystemLibrary("pthread");
-        lib.linkSystemLibrary("GLX");
-        lib.linkSystemLibrary("X11");
-        lib.linkSystemLibrary("Xcursor");
-        lib.linkSystemLibrary("Xext");
-        lib.linkSystemLibrary("Xfixes");
-        lib.linkSystemLibrary("Xi");
-        lib.linkSystemLibrary("Xinerama");
-        lib.linkSystemLibrary("Xrandr");
-        lib.linkSystemLibrary("Xrender");
+    b.installArtifact(lib);
+}
+
+fn linkLibraries(compile: *std.Build.Step.Compile, target: ResolvedTarget) void {
+    switch (target.result.os.tag) {
+        .linux => {
+            compile.linkSystemLibrary("m");
+            compile.linkSystemLibrary("pthread");
+            compile.linkSystemLibrary("GLX");
+            compile.linkSystemLibrary("X11");
+            compile.linkSystemLibrary("Xcursor");
+            compile.linkSystemLibrary("Xext");
+            compile.linkSystemLibrary("Xfixes");
+            compile.linkSystemLibrary("Xi");
+            compile.linkSystemLibrary("Xinerama");
+            compile.linkSystemLibrary("Xrandr");
+            compile.linkSystemLibrary("Xrender");
+        },
+        else => return,
+    }
+}
+
+fn collectSources(b: *std.Build, directory: []const u8) ![]const []const u8 {
+    var sources = try std.ArrayList([]const u8).initCapacity(b.allocator, 16);
+    errdefer sources.deinit(b.allocator);
+
+    var dir = try std.fs.cwd().openDir(directory, .{ .iterate = true });
+    defer dir.close();
+
+    var iter = dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind != .file) continue;
+        if (std.mem.lastIndexOf(u8, entry.name, ".c") == null) continue;
+
+        try sources.append(b.allocator, b.pathJoin(&.{ directory, entry.name }));
     }
 
-    b.installArtifact(lib);
+    return sources.toOwnedSlice(b.allocator);
 }
