c2e410decbec7a4f4735d01b0f28992f374bf600
Serhii Pikhota <serhii.pikhota@gmail.com>
2026-03-09 19:50:25 +0100

    Adjust settings and add rotating compound shapes to the scenario

--- runner/scenarios/compounds.c
+++ runner/scenarios/compounds.c
@@ -8,23 +8,35 @@ void scenario_initialize(program_config* config, physics_config *physics_config)
 
   physics_config->gravity = zero();
   physics_config->angular_damping = 1;
+  physics_config->sleep_base_bias = 1;
 }
 
 void scenario_setup_scene(physics_world *world) {
   body_shape shapes[] = {
     (body_shape) { .type = SHAPE_BOX, .box = { .size = (v3) { .x = 0.3, .y = 3, .z = 0.3 } }, .offset = zero(), .rotation = qidentity() },
-    (body_shape) { .type = SHAPE_CYLINDER, .cylinder = { .height = 2, .radius = 0.5 }, .offset = (v3) { .x = 0, .y = 1.75, .z = 0 }, .rotation = QuaternionFromEuler(PI * 0.5, 0, 0) }
+    (body_shape) { .type = SHAPE_CYLINDER, .cylinder = { .height = 2, .radius = 0.5 }, .offset = (v3) { .x = 0, .y = 1.75, .z = 0 }, .rotation = QuaternionFromEuler(PI * 0.5, 0, 0) },
+    (body_shape) { 0 },
   };
-  float masses[] = { 5.0, 3.0 };
-
-  // body b = physics_add_box_dynamic(world, 5, (v3) { .x = 0.3, .y = 3, .z = 0.3 });
-  // *b.position = (v3) { 0, 5, 0 };
+  float masses[] = { 5.0, 3.0, 1.0 };
 
   body b = physics_add_compound_body_dynamic(world, shapes, masses, 2);
   *b.position = (v3) { 0, 10, 0 };
-  *b.angular_momentum = (v3) { 0.1, 13.7, 1.8 };
+  *b.angular_momentum = (v3) { 0, 0, 36 };
 
   physics_awaken_body(world, b.handle);
+
+  shapes[0] = (body_shape) { .type = SHAPE_CYLINDER, .cylinder = { .radius = 0.3, .height = 3 }, .offset = zero(), .rotation = QuaternionFromEuler(PI * 0.5, 0, 0) };
+  shapes[1] = (body_shape) { .type = SHAPE_SPHERE, .sphere = { .radius = 0.7 }, .offset = (v3) { .x = 0, .y = 0, .z = 1.5 }, .rotation = qidentity() };
+  shapes[2] = (body_shape) { .type = SHAPE_SPHERE, .sphere = { .radius = 0.7 }, .offset = (v3) { .x = 0, .y = 0, .z = -1.5 }, .rotation = qidentity() };
+
+  masses[2] = 5;
+
+  b = physics_add_compound_body_dynamic(world, shapes, masses, 3);
+  *b.position = (v3) { 15, 10, 0};
+  *b.angular_momentum = (v3) { 0, 50, 0};
+
+  physics_awaken_body(world, b.handle);
+
 }
 
 void scenario_handle_input(physics_world *world, Camera *camera) {
